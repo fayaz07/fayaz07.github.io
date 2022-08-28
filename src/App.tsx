1b8c189fc32b5f2c7f4189880787ae51ab293e9f
Mohammad Fayaz <fayazfz07@gmail.com>
2022-08-28 18:13:42 +0530

    minimal dev portfolio

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import github from "./assets/github.png";
+import linkedin from "./assets/linkedin.png";
+import dev from "./assets/dev.png";
+import twitter from "./assets/twitter.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="App col center">
+      <img src={dev} className="dev-image" alt="dev" />
+      <p className="greet">Hi there,</p>
+      <p>
+        <span className="iam">I'm</span>{" "}
+        <span className="name">Mohammad Fayaz</span>
+      </p>
+      <p className="subtitle">a software engineer</p>
+      <div className="row bottom-row">
+        <a className="row plink" href="https://www.linkedin.com/in/fayaz07/">
+          <p className="tagText">say hi on</p>{" "}
+          <img src={linkedin} className="tagImg" alt="linkedin" />
         </a>
-      </header>
+
+        <a className="row plink" href="https://github.com/fayaz07">
+          <p className="tagText">fork on</p>{" "}
+          <img src={github} className="tagImg" alt="github" />
+        </a>
+
+        <a className="row plink" href="https://twitter.com/fayaz_7_">
+          <p className="tagText">tag on</p>{" "}
+          <img src={twitter} className="tagImg" alt="twitter" />
+        </a>
+      </div>
     </div>
   );
 }
