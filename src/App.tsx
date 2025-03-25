af777cc994232274c48407a5b3a264923653bf7f
Mohammad Fayaz <fayazfz07@gmail.com>
2025-03-25 19:12:37 +0530

    fix build dir and contain image css

--- src/App.tsx
+++ src/App.tsx
@@ -7,30 +7,32 @@ import PalestineBanner from "./banners/Palestine";
 
 function App() {
   return (
-    <div className="App col center">
+    <div>
       <PalestineBanner />
-      <img src={dev} className="dev-image" alt="dev" />
-      <p className="greet">Hi there,</p>
-      <p>
-        <span className="iam">I'm</span>{" "}
-        <span className="name">Mohammad Fayaz</span>
-      </p>
-      <p className="subtitle">a software engineer</p>
-      <div className="row bottom-row">
-        <a className="row plink" href="https://www.linkedin.com/in/fayaz07/">
-          <p className="tagText">say hi on</p>{" "}
-          <img src={linkedin} className="tagImg" alt="linkedin" />
-        </a>
+      <div className="App col center">
+        <img src={dev} className="dev-image" alt="dev" />
+        <p className="greet">Hi there,</p>
+        <p>
+          <span className="iam">I'm</span>{" "}
+          <span className="name">Mohammad Fayaz</span>
+        </p>
+        <p className="subtitle">a software engineer</p>
+        <div className="row bottom-row">
+          <a className="row plink" href="https://www.linkedin.com/in/fayaz07/">
+            <p className="tagText">say hi on</p>{" "}
+            <img src={linkedin} className="tagImg" alt="linkedin" />
+          </a>
 
-        <a className="row plink" href="https://github.com/fayaz07">
-          <p className="tagText">fork on</p>{" "}
-          <img src={github} className="tagImg" alt="github" />
-        </a>
+          <a className="row plink" href="https://github.com/fayaz07">
+            <p className="tagText">fork on</p>{" "}
+            <img src={github} className="tagImg" alt="github" />
+          </a>
 
-        <a className="row plink" href="https://twitter.com/fayaz_7_">
-          <p className="tagText">tag on</p>{" "}
-          <img src={twitter} className="tagImg" alt="twitter" />
-        </a>
+          <a className="row plink" href="https://twitter.com/fayaz_7_">
+            <p className="tagText">tag on</p>{" "}
+            <img src={twitter} className="tagImg" alt="twitter" />
+          </a>
+        </div>
       </div>
     </div>
   );
--- src/banners/Palestine.tsx
+++ src/banners/Palestine.tsx
@@ -1,10 +1,20 @@
 export default function PalestineBanner() {
   return (
-    <div style={{ top: 0, width: "100%" }}>
+    <div
+      style={{
+        top: 0,
+        width: "100vw",
+        marginLeft: 0,
+        marginRight: 0,
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
       <a href="https://github.com/Safouene1/support-palestine-banner/blob/master/Markdown-pages/Support.md">
         <img
           src="https://raw.githubusercontent.com/Safouene1/support-palestine-banner/master/banner-project.svg"
           alt="Support Palestine"
+          style={{ objectFit: "contain", maxWidth: "100vw" }}
         />
       </a>
     </div>
