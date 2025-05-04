220aa75da493ebd2af865ac48660a136d506f9a5
Mohammad Fayaz <35001172+fayaz07@users.noreply.github.com>
2025-05-04 17:04:18 +0530

    add back removed file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
